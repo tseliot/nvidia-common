215355d210f60df3885f1db97606513beec0c0a5
Alberto Milone <alberto.milone@canonical.com>
2012-09-20 16:07:38 +0200

    Revert "NvidiaDetector/nvidiadetector.py: add support for -experimental flavours"
    This reverts commit c6a8fbc5656b34de47b3990fb83884ef61dcbede.

--- NvidiaDetector/nvidiadetector.py
+++ NvidiaDetector/nvidiadetector.py
@@ -133,8 +133,7 @@ class NvidiaDetection(object):
 
         for package in apt.Cache():
             if (not package.name.startswith('nvidia-')
-                or 'updates' in package.name
-                or 'experimental' in package.name):
+                or 'updates' in package.name):
                 continue
             try:
                 m = package.candidate.record['Modaliases']
